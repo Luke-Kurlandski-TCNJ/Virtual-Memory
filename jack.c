1e89b4184496b7a5741b4803f24fd3a843ccadff
Jack <tayloj22@tcnj.edu>
2020-04-01 16:15:08 -0400

    Created separate workspace for myself

---
+++ jack.c
@@ -0,0 +1,22 @@
+/*
+   Jack Taylor and Luke Kurlandski
+   Dr. Yoon
+   CSC345: Operating Systems
+   Project 3: Virtual Memory
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int page_number(int address) {
+	return (65280 & address); // 65280 == 00..001111111100..00
+}
+
+int offset(int address) {
+	return (255 & address); // 255 == 00...0011111111
+}
+
+int main(int *argc, char **argv) {
+	printf("%d\n", page_number(1));
+	return 0;
+}
