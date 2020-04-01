fd6e97e6ca950885224c7ad72af7b7632e440a76
Jack <tayloj22@tcnj.edu>
2020-04-01 16:44:03 -0400

    Addresses.txt parsing

--- jack.c
+++ jack.c
@@ -16,7 +16,32 @@ int offset(int address) {
 	return (255 & address); // 255 == 00...0011111111
 }
 
+void addressParsing() {
+	FILE* file;
+	int maxChar = 10;
+	char str[maxChar];
+	// open txt file containing all logical addresses
+	file = fopen("addresses.txt", "r");
+	
+	// return if file could not open
+	if (file == NULL) {
+		printf("Could not open addresses.txt\n");
+		return;
+	}
+
+	// parse through each line, getting info from each logical address
+	while (fgets(str, maxChar, file) != NULL) {
+		int currAddress = atoi(str);
+		printf("page number = %d\n", page_number(currAddress));
+		printf("offset = %d\n", offset(currAddress));
+	}
+
+	return;
+}
+
 int main(int *argc, char **argv) {
+	addressParsing();
+
 	printf("%d\n", page_number(1));
 	return 0;
 }
