19cc4c1d90d2434556fda81fa7d34bb3efabd972
Luke <kurlanl1@tcnj.edu>
2020-04-01 16:48:43 -0400

    TLB and table

--- luke.c
+++ luke.c
@@ -7,13 +7,22 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
+
+// index = page number, value = page offset
+int PAGE_TABLE = [pow(2,8)];
+int TLB = [16];
+
+
 
 int page_number(int address) {
-	return (65280 & address); // 65280 == 00..001111111100..00
+	// 65280 = 000000001111111100000000
+	return (65280 & address);
 }
 
 int offset(int address) {
-	return (255 & address); // 255 == 00...0011111111
+	// 255 = 000000000000000011111111
+	return (255 & address);
 }
 
 int main(int *argc, char **argv) {
