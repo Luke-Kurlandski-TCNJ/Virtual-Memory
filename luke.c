57c81de9156358b4a84f853bdd9ed8e6c2f1439b
Luke <kurlanl1@tcnj.edu>
2020-04-01 19:11:37 -0400

    non empty message

--- luke.c
+++ luke.c
@@ -9,14 +9,16 @@
 #include <stdio.h>
 #include <math.h>
 
-// index = page number, value = page offset, valid/invalid
-int PAGE_TABLE[256][2];
+// index:page number
+// value:(0:frame number, 1:page offset, 2:valid/invalid bit)
+int PAGE_TABLE[256][3];
+// index:nothing, value:(0:page number, 1:frame number)
 int TLB[16][2];
 
 // Calculate the page number from a logical address.
 int page_number(int address) {
-	// 65280 = 000000001111111100000000
-	return (65280 & address);
+	// 255 = 000000001111111100000000
+	return (255 & address>>8);
 }
 
 // Calculate the offset for a logical address.
@@ -25,24 +27,24 @@ int page_offset(int address) {
 	return (255 & address);
 }
 
-// Return a 0 if the page number is in the TLB.
-int search_TLB(int number, int offset) {
+// Return the frame number if the page number is in the TLB.
+int search_TLB(int number) {
 	for (int i=0; i<16; i++) {
-		if (TLB[i][0] == number && TLB[i][0] == offset)
-			return 0;
+		if (TLB[i][0] == number)
+			return TLB[i][1];
 	}
 	return -1;
 }
 
-// Return a 0 if the page number is in the page table.
-int search_table(int number, int offset) {
-	if (PAGE_TABLE[number][1] == 1) 
-		return 0;
-	else 
+// Return the frame number if the page number is in page table.
+int search_table(int number) {
+	if (PAGE_TABLE[number][2] == 1)  
+		return PAGE_TABLE[number][0];
+	else // Page Fault 
 		return -1;
 }
 
-void addressParsing() {
+void addressParsing(char *f) {
 	FILE* file;
 	int maxChar = 10;
 	char str[maxChar];
@@ -60,21 +62,21 @@ void addressParsing() {
 		int address = atoi(str);
 		int number = page_number(address);
 		int offset = page_offset(address);
+		//printf("Number: %d Offset: %d\n", number, offset);
+		
 		// Search the TLB for the page.
-		if (search_TLB(number, offset) < 0) {
-			// Not found in TLB, so we search page table.
-			if (search_table(number, offset) < 0) {
-				// Not found in TLB, so read from memory.
-			}
+		frame_number = search_TLB(number);
+		if (frame_number < 0) {
+			// Search page table for page.
+			frame_number = search_table(number);
+			if (frame_number < 0)
+				// Read from memory.
 		}
 	}
-
 	return;
 }
 
-int main(int *argc, char **argv) {
-	printf("%d\n", page_number(1));
+int main(int *argc, char **argv) {	
+	addressParsing(argv[1]);
 	return 0;
 }
-
-
